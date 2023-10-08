8ba95cbbdb1ea082d2122b74e6d41fcbaaf3d2e5
maybaron1 <may123baron@gmail.com>
2023-10-08 21:56:05 +0300

    code writing

--- app/src/main/java/com/example/eggapp/MainActivity.java
+++ app/src/main/java/com/example/eggapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.eggapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private int counter;
@@ -12,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         counter = 10;
+        TextView counterTextView = findViewById(R.id.textview_counter);
+        counterTextView.setText(""+counter);
     }
 }
